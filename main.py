83df4bc4efec84a96d360bdee8f666798b8327c2
zhengjunhao <changchunjunhao@gmail.com>
2018-11-22 00:46:28 +0800

    (v0.2) able to take screenshot and detect where to click

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python3
 
-import os
+import os, time
 
-from script import graphic, adb
+from script import graphic, adb, cli
 
 def main():
+    logTime('script begin')
+    assert adb.hasDevice()
+    logTime('hasDevice spends')
+
     file = getScreenShot()
-    graphic.getHitPositions(file, 'asserts/cake3.png', 0.56, True)
+    logTime('screenshot spends')
+    # screenSize = adb.getScreenSize()
+    screenSize = (1080, 1920)
+    logTime('screensize spends')
+    positions = graphic.getHitPositions(file, screenSize=screenSize)
+    logTime('analysis hit positions spends')
+    graphic.drawDots(file, positions, targetFile='build/result.png')
+    logTime('drawDots spends')
+    cli.execute('open build/result.png', '.')
+    logTime('open result file spends')
+
+    logTime('done')
+
+lastTime = 0
+def logTime(msg):
+    global lastTime
+    currentTime = time.time()
+    if lastTime:
+        print('%s  %s' % (msg, currentTime - lastTime)) 
+    else:
+        print(msg)
+    lastTime = currentTime
 
 def getScreenShot():
     ssFile = 'build/s.png'
     adb.screenShot(ssFile)
     assert os.path.exists(ssFile)
     return ssFile
+
 
 if __name__ == '__main__':
     main()
--- script/graphic.py
+++ script/graphic.py
@@ -1,24 +1,59 @@
 import cv2, os
 import numpy as np
 
-def getHitPositions(searchFile, templateFile, threshold, paintIt=False):
+def drawDots(file, positions, targetFile):
+    assert os.path.exists(file)
+    image = cv2.imread(file)
+    for pt in positions:
+        cv2.circle(image, (int(pt[0]), int(pt[1])), 5, (0,0,255), -1)
+    cv2.imwrite(targetFile, image)
+
+def getHitPositions(searchFile, screenSize=(1080, 1920)):
+    cakeFiles = ['asserts/cake1.png', 'asserts/cake2.png', 'asserts/cake3.png']
     searchImage = getRedChannel(getImage(searchFile))
-    templateImage = getRedChannel(getImage(templateFile))
 
     result = []
+    for f in cakeFiles:
+        print('checking: %s' % f)
+        templateImage = getRedChannel(getImage(f))
+        for pt in _getHitPositions(searchImage, templateImage, screenSize=screenSize):
+            result.append(pt)
+
+    return result
+
+def _getHitPositions(searchImage, templateImage, threshold=0.56, screenSize=(1080, 1920), debug=False):
+    if not screenSize == (1080, 1920):
+        w, h = templateImage.shape[::-1]
+        newWidth, newHeight = int(w * screenSize[0] / 1080), int(h * screenSize[1] / 1920)
+        templateImage = cv2.resize(templateImage, (newWidth, newHeight))
 
     res = cv2.matchTemplate(searchImage, templateImage, cv2.TM_CCOEFF_NORMED)
 
+
+    w, h = templateImage.shape[::-1]
+    halfW, halfH = int(w/2), int(h/2)
+    def square(x):
+        return x * x
+    def absolute(x):
+        return x if x > 0 else -x
+    suggestingDistance = 80
+    def tooNear(existingPoints, pt):
+        if debug:
+            print('current points: %s' % existingPoints)
+        for _pt in existingPoints:
+            dist = absolute(_pt[0] - pt[0]) + absolute(_pt[1] - pt[1])
+            if debug:
+                print('dist: %s' % dist)
+            if dist < suggestingDistance:
+                return True
+        return False
+
+    result = []
     loc = np.where(res >= threshold)
     for pt in zip(*loc[::-1]):
-        result.append(pt)
-
-    if paintIt:
-        w, h = templateImage.shape[::-1]
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(searchImage, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-        cv2.imwrite('build/result.png', searchImage)
-
+        center = (pt[0] + halfW, pt[1] + halfH)
+        if not tooNear(result, center):
+            result.append(center)
     return result
 
 def getRedChannel(im):
